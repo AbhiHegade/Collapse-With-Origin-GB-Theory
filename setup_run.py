297ae52586c68d127bdb70289b2c07152e5f2c92
AbhiHegade <abhikumbale@gmail.com>
2022-07-12 19:38:40 -0500

    Changes to animation script

--- setup_run.py
+++ setup_run.py
@@ -7,8 +7,9 @@ from datetime import datetime
 import os
 #===============================================================================
 theory = "gaussian"
-Amps = np.concatenate((np.linspace(1e-3,1e-2,10), np.linspace(1e-2,1e-1,10), np.linspace(1e-1,1)))
-np.unique(Amps)
+# Amps = np.concatenate((np.linspace(1e-3,1e-2,10), np.linspace(1e-2,1e-1,10), np.linspace(1e-1,1)))
+# np.unique(Amps)
+Amps = np.array([0.357143])
 ls = np.array([0.1])
 
 if theory == "shift_symm":
@@ -28,9 +29,9 @@ sim = Sim()
 sim.slurm = False
 # sim.animscript = "/Users/abhi/Work/Projects/Hyperbolitcity-Gravitational-Collapse/code-f-phi/Animation-Script.ipynb"
 sim.animscript = "./Animation-Script.ipynb"
-sim.nx = 10000
-sim.nt = 10000
-sim.save_steps = int(sim.nt/1000)
+sim.nx = 6000
+sim.nt = 24000
+sim.save_steps = 10
 sim.search =False
 # sim.out_dir = "/Users/abhi/Work/Projects/Hyperbolitcity-Gravitational-Collapse/code-f-phi/output/Phase-Space/Shift-Symmetric-Theory/Run_nx_{}_nt_".format(sim.nx,sim.nt)+ current_time.strftime("%a")+"_"+current_time.strftime("%b")+"_"+ str(current_time.day) +"_"+ str(current_time.hour) + "_"+str(current_time.minute)
 if sim.search == True:
@@ -142,7 +143,11 @@ else:
                 f.write("Data saved at:{} \n".format(sim.out_dir))
     else:
         if __name__ == '__main__':
-            pool_nums = 6
+            if len(input_data) >=6:
+                pool_nums = 6
+            else :
+                pool_nums = len(input_data)
+            print("pool_nums = ", pool_nums)
             t_start = time.time()
             print("Starting multiprocessing pool..")
             pool = Pool(pool_nums)
